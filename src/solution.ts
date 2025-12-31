825bff48c2f593b1fd75f1189dc444677c5df252
Sakif-Hridoy <sakifworkspace@gmail.com>
2025-12-31 15:28:07 +0600

    Problem Three solution

--- src/solution.ts
+++ src/solution.ts
@@ -30,17 +30,41 @@ function formatValue(param:DataType){
 type stringOrArray = string | any[];
 
 // give type to function
-function getLength(value:stringOrArray):number{
-    if (typeof value === 'string'){
-        return value.length
-    }
-    if(Array.isArray(value)){
-        return value.length
-    }
+function getLength(value: stringOrArray): number {
+  if (typeof value === "string") {
+    return value.length;
+  }
+  if (Array.isArray(value)) {
+    return value.length;
+  }
 
-    return 0
+  return 0;
 }
-
 
 // console.log(getLength("typescript"));
 // console.log(getLength([10, 20, 30, 40]));
+
+/* Problem Three Solution */
+
+// define class
+class Person {
+  name: string;
+  age: number;
+
+  // define constructor
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  // methods
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
+
+// const person1 = new Person('John Doe', 30);
+// console.log(person1.getDetails());
+
+// const person2 = new Person('Alice', 25);
+// console.log(person2.getDetails());
